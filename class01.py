490f4cdf823be06cb50210538935863ca8c2b803
unknown <2667105387@qq.com>
2021-07-03 20:19:01 +0800

    第二次尝试提交，是否成功

---
+++ class01.py
@@ -0,0 +1,15 @@
+# _*_ coding:utf-8 _*_
+# 作者：hungryboy
+# @Time: 2021/1/28
+# @File: class01.py
+
+print("欢迎来到hungryboy的python世界！")
+
+def test():
+    print("这是一个函数，在Demo类外面")
+
+class Demo:
+    print("this is a demo")
+
+    def demo(self):
+        print("这是Demo类中的方法！")
